4a7a50ab94c03b8f0f87fc6403acaa8b25c5f675
Andrew <njayaandrew@gmail.com>
2018-11-27 05:05:39 +0300

    [feature] created an endpoint to add a user

--- tests/test_endpoints/test_admin.py
+++ tests/test_endpoints/test_admin.py
@@ -21,38 +21,43 @@ class Test_admin:
         return token
 
 
-    def header(self,generate_token):
-        headers = {'X-APP-SECRET': '{}'.format(generate_token)}
-        return headers
+
+        
+
 
     def test_post(self,cli_ent,generate_token):
-        response=cli_ent.post('api/v1/admin/products',headers=self.header(generate_token),data=json.dumps(dict(product_name='Timberland shoes',
+        headers = {'X-APP-SECRET': '{}'.format(generate_token)}
+        response=cli_ent.post('api/v1/admin/products',headers=headers,data=json.dumps(dict(product_name='Timberland shoes',
         price=40,quantity=10)), content_type="application/json")
         data=json.loads(response.data)
         assert response.status_code==200
         assert "The product has been added" in data["message"]
 
     def test_post_empty_string(self,cli_ent,generate_token):
-        empty_response=cli_ent.post('api/v1/admin/products',headers=self.header(generate_token),data=json.dumps(dict(product_name='',
+        headers = {'X-APP-SECRET': '{}'.format(generate_token)}
+        empty_response=cli_ent.post('api/v1/admin/products',headers=headers,data=json.dumps(dict(product_name='',
         price=40,quantity=10)), content_type="application/json")
         empty_data=json.loads(empty_response.data)
         assert "Please provide all values" in empty_data["message"]
         
     def test_post_invalid_values(self,cli_ent,generate_token):
-        invalid_response=cli_ent.post('api/v1/admin/products',headers=self.header(generate_token),data=json.dumps(dict(product_name=123,
+        headers = {'X-APP-SECRET': '{}'.format(generate_token)}
+        invalid_response=cli_ent.post('api/v1/admin/products',headers=headers,data=json.dumps(dict(product_name=123,
         price=40,quantity=10)), content_type="application/json")
         invalid_data=json.loads(invalid_response.data)
         assert "Please provide valid strings or integers" in invalid_data["message"]
 
 
     def test_get(self,cli_ent,generate_token):
-        response=cli_ent.get('/api/v1/admin/products',headers=self.header(generate_token))
+        headers = {'X-APP-SECRET': '{}'.format(generate_token)}
+        response=cli_ent.get('/api/v1/admin/products',headers=headers)
         data=json.loads(response.data)
         assert response.status_code==200
         assert data=={'Products':admin.p.get_products()}
 
     def test_get_one_product(self,cli_ent,generate_token):
-        response=cli_ent.get('/api/v1/admin/products/'+str(1),headers=self.header(generate_token))
+        headers = {'X-APP-SECRET': '{}'.format(generate_token)}
+        response=cli_ent.get('/api/v1/admin/products/'+str(1),headers=headers)
         data=json.loads(response.data)
         assert response.status_code==200
         assert data=={'Product':admin.p.get_a_product(product_id=1)}
