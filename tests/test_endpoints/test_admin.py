9d12547886d8b947a090cb844fc1167641a7d37d
Andrew <njayaandrew@gmail.com>
2018-11-27 05:39:35 +0300

    [feature] created an endpoint to add a user

--- tests/test_endpoints/test_admin.py
+++ tests/test_endpoints/test_admin.py
@@ -27,7 +27,7 @@ class Test_admin:
 
     def test_post(self,cli_ent,generate_token):
         headers = {'X-APP-SECRET': '{}'.format(generate_token)}
-        response=cli_ent.post('api/v1/admin/products',headers=headers,data=json.dumps(dict(product_name="Timberland shoes",
+        response=cli_ent.post('/api/v1/admin/products',headers=headers,data=json.dumps(dict(product_name="Timberland shoes",
         price=40,quantity=10)), content_type="application/json")
         data=json.loads(response.data)
         assert response.status_code==200
@@ -35,14 +35,14 @@ class Test_admin:
 
     def test_post_empty_string(self,cli_ent,generate_token):
         headers = {'X-APP-SECRET': '{}'.format(generate_token)}
-        empty_response=cli_ent.post('api/v1/admin/products',headers=headers,data=json.dumps(dict(product_name='',
+        empty_response=cli_ent.post('/api/v1/admin/products',headers=headers,data=json.dumps(dict(product_name='',
         price=40,quantity=10)), content_type="application/json")
         empty_data=json.loads(empty_response.data)
         assert "Please provide all values" in empty_data["message"]
         
     def test_post_invalid_values(self,cli_ent,generate_token):
         headers = {'X-APP-SECRET': '{}'.format(generate_token)}
-        invalid_response=cli_ent.post('api/v1/admin/products',headers=headers,data=json.dumps(dict(product_name=123,
+        invalid_response=cli_ent.post('/api/v1/admin/products',headers=headers,data=json.dumps(dict(product_name=123,
         price=40,quantity=10)), content_type="application/json")
         invalid_data=json.loads(invalid_response.data)
         assert "Please provide valid strings or integers" in invalid_data["message"]
