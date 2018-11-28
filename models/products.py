37ff235edbac6a69e88aca52971c74768431c403
Andrew <njayaandrew@gmail.com>
2018-11-29 02:59:56 +0300

    [chore] added connection object on models

--- models/products.py
+++ models/products.py
@@ -12,14 +12,14 @@ class products():
         self.con=self.cur_con[1]
 
     def add_product(self):
-        insert_sql="""INSERT INTO products(product_name,product_price,product_qty) VALUES(%s,%s,%s)"""
+        insert_sql="INSERT INTO products(product_name,product_price,product_qty) VALUES(%s,%s,%s)"
         product_data=(self.p_name,self.price,self.quantity)
         self.cur.execute(insert_sql,product_data)
         self.con.commit()
         return 'The product has been added'
 
     def get_a_product(self, product_id):
-        product_query="""SELECT * FROM products WHERE product_id=%s"""
+        product_query="SELECT * FROM products WHERE product_id=%s"
         self.cur.execute(product_query,[product_id])
         prodct=self.cur.fetchone()
         if prodct:
@@ -28,7 +28,7 @@ class products():
             return "The product wasn\'t found."
            
     def get_products(self):
-        products_query="""SELECT * FROM products"""
+        products_query="SELECT * FROM products"
         self.cur.execute(products_query)
         products_table_rows=self.cur.fetchall()
         if products_table_rows:
@@ -40,7 +40,7 @@ class products():
             return 'There are no products yet'
     def update_product(self,product_id,product_name,price,qty):
         try:
-            update_psql="""UPDATE products SET product_name=%s, product_price=%s, product_qty=%s WHERE product_id=%s RETURNING product_id,product_name,product_price"""
+            update_psql="UPDATE products SET product_name=%s, product_price=%s, product_qty=%s WHERE product_id=%s RETURNING product_id,product_name,product_price"
             self.cur.execute(update_psql,(product_name,price,qty,product_id))
             updated_product=self.cur.fetchone()
             if updated_product==None:
@@ -50,7 +50,7 @@ class products():
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
     def delete_product(self,product_id):
-        delete_product_psql="""DELETE FROM products WHERE product_id=%s RETURNING product_id"""
+        delete_product_psql="DELETE FROM products WHERE product_id=%s RETURNING product_id"
         self.cur.execute(delete_product_psql, [product_id])
         returned_id=self.cur.fetchone()
         self.con.commit()
