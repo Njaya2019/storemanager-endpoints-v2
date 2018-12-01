2d7c06c3edb78316d2d288832a88c454e7d8159f
Andrew <njayaandrew@gmail.com>
2018-12-02 02:09:43 +0300

    [chore] Added tests to to test user endpoint registration and login

--- tests/test_endpoints/test_add_user.py
+++ tests/test_endpoints/test_add_user.py
@@ -4,6 +4,9 @@ from models.products import products
 from models.users import users
 import jwt
 import datetime
+import psycopg2
+import psycopg2.extras as p_extras
+from werkzeug.security import generate_password_hash
 class Test_add_user:
     @pytest.fixture(scope='module')
     def cli(self):
@@ -12,7 +15,13 @@ class Test_add_user:
     
     @pytest.fixture(scope='module')
     def generate_token(self,cli):
-        rv=cli.post('/api/v1/admin/login', data=json.dumps(dict(user_email='njayaandrew@andela.com',user_password='1234')), content_type="application/json")
+        con=psycopg2.connect(dbname='postgres', user='postgres', host='localhost', password=' ')
+        cur=con.cursor(cursor_factory=p_extras.DictCursor)
+        hashed_pswd = generate_password_hash('1234',method='sha256')
+        add_user_psql="INSERT INTO users(user_fullname,user_email,user_role,user_password) VALUES(%s,%s,%s,%s)"
+        cur.execute(add_user_psql,('Andrew Njaya','njayaandrew@companyname.com','Admin',hashed_pswd))
+        con.commit()
+        rv=cli.post('/api/v1/admin/login', data=json.dumps(dict(user_email='njayaandrew@companyname.com',user_password='1234')), content_type="application/json")
         data=json.loads(rv.data)
         token=data['token']
         return token
@@ -30,7 +39,7 @@ class Test_add_user:
     def test_post_confirm_password(self,cli,generate_token):
         headers = {'X-APP-SECRET':'{}'.format(generate_token)}
         response=cli.post('/api/v1/admin/signup',headers=headers,data=json.dumps(dict(user_fullname='Darius ndubi',
-        user_email='njayaandrew@companyname.com',user_role='attendant',user_password='secret12',user_confirm_pwd='secret')), content_type="application/json")
+        user_email='njayaandrew@companynames.com',user_role='attendant',user_password='secret12',user_confirm_pwd='secret')), content_type="application/json")
         data=json.loads(response.data)
         assert response.status_code==200
         assert "Passwords do not match" in data["message"]
